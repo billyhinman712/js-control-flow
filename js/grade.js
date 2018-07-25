325bcbc24b5c141bed9b29a8bb9539ec36cc80e3
billyhinman712 <41489170+billyhinman712@users.noreply.github.com>
2018-07-25 15:49:54 -0700

    js practice

---
+++ js/grade.js
@@ -0,0 +1,19 @@
+var grade = 77
+
+switch(true){
+	case (grade < = 100 && grade > 90):
+	console.log("A");
+	break;
+	case (grade < = 90 && grade > 80):
+	console.log("B");
+	break;
+	case (grade < = 80 && grade > 70):
+	console.log("C");
+	break;
+	case (grade < = 70 && grade > 60):
+	console.log("D");
+	break;
+	default:
+	console.log("F");
+	break;
+}
---
+++ js/tempConvert.js
@@ -0,0 +1,4 @@
+var x = 45
+
+var y = (x-32)*(5/9);
+console.log(x  +" degrees Fahrenheit is " + y + " degrees Celsius")
